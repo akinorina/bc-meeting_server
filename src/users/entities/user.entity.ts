6b8bc67a425cb813cf8af19d6f7c186b46f6d6ce
Akinori Nakata <akinori.nakata@bright-century.com>
2024-11-07 15:05:16 +0900

    不要な記述を削除

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -21,7 +21,6 @@ import { Blog } from '../../blogs/entities/blog.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { VerifyingEmail } from './verify-email.entity';
 import { Room } from '../../rooms/entities/room.entity';
-// import { RoomAttender } from '../../rooms/entities/room_attender.entity';
 
 @Entity()
 export class User {
@@ -68,9 +67,6 @@ export class User {
 
   @OneToMany(() => Room, (room) => room.user)
   rooms: Room[];
-
-  // @OneToMany(() => RoomAttender, (room_attender) => room_attender.attender)
-  // room_attenders: RoomAttender[];
 
   @CreateDateColumn({ comment: '作成日時' })
   createdAt: string | undefined = undefined;
